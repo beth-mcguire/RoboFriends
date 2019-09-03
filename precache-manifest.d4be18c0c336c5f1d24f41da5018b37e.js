445861bcf326a385d31c39657b07b78dfb1d78dd
unknown <beth.mcguire1221@gmail.com>
2019-09-03 14:26:17 -0700

    Added all files

---
+++ precache-manifest.d4be18c0c336c5f1d24f41da5018b37e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15c435d1ca8c6e5ea83f98c322401fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "9bdd23aaea2f1f3e774b",
+    "url": "/static/css/main.b4b555c6.chunk.css"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/static/js/2.80c069d3.chunk.js"
+  },
+  {
+    "revision": "9bdd23aaea2f1f3e774b",
+    "url": "/static/js/main.0c418f64.chunk.js"
+  },
+  {
+    "revision": "31175d55b7ad255ef221",
+    "url": "/static/js/runtime~main.6237261c.js"
+  }
+]);
